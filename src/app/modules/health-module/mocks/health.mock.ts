cbf1215b8c375d38a9cfd21ae23f5284b8a2eead
Анастасия Полякова <a.polyakova@isimplelab.com>
2026-02-23 14:15:57 +0300

    создание интерфесов в разделе "Здоровье"

--- src/app/modules/health-module/mocks/health.mock.ts
+++ src/app/modules/health-module/mocks/health.mock.ts
@@ -1,4 +1,4 @@
-import { HealthDictionary, HealthSection } from "../models/health.model";
+import { Health, HealthDictionary, HealthSection } from "../models/health.model";
 
 export const HealthSectionsMock: HealthSection[] = [
   {
@@ -36,8 +36,17 @@ export const HealthSectionsMock: HealthSection[] = [
     dictionary: false,
   },
   {
-    id: 'water',
+    id: 'blood-sugar',
     order: 5,
+    name: 'Сахар',
+    hide: false,
+    type: 'number',
+    defaultValue: '1,5',
+    dictionary: false,
+  },
+  {
+    id: 'water',
+    order: 6,
     name: 'Вода',
     hide: false,
     type: 'number',
@@ -46,7 +55,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'temperature',
-    order: 6,
+    order: 7,
     name: 'Температура',
     hide: false,
     type: 'number',
@@ -55,7 +64,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'other',
-    order: 7,
+    order: 8,
     name: 'Другое',
     hide: false,
     type: 'type',
@@ -63,7 +72,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'symptoms',
-    order: 8,
+    order: 9,
     name: 'Симптомы',
     hide: false,
     type: 'type',
@@ -71,7 +80,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'digestion',
-    order: 9,
+    order: 10,
     name: 'Пищеворение',
     hide: false,
     type: 'type',
@@ -79,7 +88,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'sex',
-    order: 10,
+    order: 11,
     name: 'Секс',
     hide: false,
     type: 'type',
@@ -87,7 +96,7 @@ export const HealthSectionsMock: HealthSection[] = [
   },
   {
     id: 'monthlies',
-    order: 10,
+    order: 12,
     name: 'Женский цикл',
     hide: false,
     type: 'day-boolean',
@@ -95,10 +104,112 @@ export const HealthSectionsMock: HealthSection[] = [
   },
 ];
 
+export const HealthMock: Health = {
+  data: '01.04.2026',
+  healthDictionary: HealthDictionary,
+  weight?: string;
+  bloodPressure?: string;
+  water?: string;
+  temperature?: string;
+  monthlies?: boolean;
+};
+
 export const HealthDictionaryMock: HealthDictionary[] = [
   {
+    id: 'calm',
+    name: 'Спокойствие',
+    healthSection: 'mood',
+    value: false,
+
+  },
+  {
+    id: 'joy',
+    name: 'Радость',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'high-energy',
+    name: 'Много энергии',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'playfulness',
+    name: 'Игривость',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'mood-swings',
+    name: 'Перепады настроения',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'irritation',
+    name: 'Раздражение',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'sadness',
+    name: 'Грусть',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'anxiety',
+    name: 'Тревога',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'depression',
+    name: 'Подавленность',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'guilt',
+    name: 'Чувство вины',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'obsessive-thoughts',
+    name: 'Навязчивые мысли',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'low-energy',
+    name: 'Мало энергии',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'apathy',
+    name: 'Апатия',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'confusion',
+    name: 'Растерянность',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
+    id: 'self-criticism',
+    name: 'Жесткая самокритика',
+    healthSection: 'mood',
+    value: false,
+  },
+  {
     id: 'mood',
-    name: 'Настроение',
-    healthSection: 'Настроение',
+    name: 'Радость',
+    healthSection: 'mood',
+    value: false,
   },
 ];
